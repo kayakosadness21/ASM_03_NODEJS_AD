a136007b7ca5c3368888371dfc9e1913d7f298e3
duy366110 <duy366110@gmail.com>
2024-03-03 12:33:01 +0700

    #Commit render template chat.

---
+++ src/components/pages/page-chat/page-chat.js
@@ -0,0 +1,32 @@
+import classes from "./page-chat.module.css";
+
+const PageChat = (props) => {
+
+    return (
+        <div className={classes['page-chat-component']}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <ul className={classes['chat-tab']}>
+                            <li className={classes['chat-tab-items']}>
+                                <span className={classes['chat-tab-tems_thumb']}>
+                                    <img src="assets/images/user_blank.png" alt="" />
+                                </span>
+                                <span>test</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="col-8">
+                        <div className={classes['chat-content']}>
+                            <div className={classes['chat-content-messages']}></div>
+                            <div className={classes['chat-content-input']}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PageChat;
