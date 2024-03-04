cc4f0ca20dd1093b8e8265783c3cdcc17dd79047
duy366110 <duy366110@gmail.com>
2024-03-04 11:23:03 +0700

    #Commit 04.03.2024: Config admin send message to client.

--- src/components/pages/page-chat/page-chat.js
+++ src/components/pages/page-chat/page-chat.js
@@ -6,6 +6,7 @@ const PageChat = (props) => {
     const shareSocket = useSelector((state) => state.storeSocket);
     const { user } = useSelector((state) => state.logInReducer);
 
+    const [client, setClient] = useState(null);
     const [listUser, setListUser] = useState([]);
     const [messages, setMessages] = useState([]);
     const messageRef = useRef();
@@ -20,9 +21,9 @@ const PageChat = (props) => {
         })
 
         shareSocket.socket?.on("CLIENT-ADMIN-CHOOSE", (data) => {
-            console.log(data);
             let { client } = data;
             if(client) {
+                setClient(client);
                 setMessages(client.message);
             }
         })
@@ -30,24 +31,30 @@ const PageChat = (props) => {
         shareSocket.socket?.on("CLIENT-SEND-MESSAGE-TO-ADMIN-SUPPORT", (data) => {
             let { user: userClient } = data;
 
-            console.log(user.email);
-            console.log(userClient.current_care)
-            console.log(user.email === userClient.current_care)
             setMessages([]);
             if(user.email == userClient.current_care) {
                 setMessages(userClient.message);
-                // console.log(user);
-                // console.log(userClient);
+            }
+        })
+
+        shareSocket.socket?.on("ADMIN-SEND-MESSAGE-TO-CLIENT-DONE", (data) => {
+            let { client } = data;
+            setMessages([]);
+            if(user.email == client.current_care) {
+                setMessages(client.message);
             }
         })
 
     }, [shareSocket.socket])
 
-    const onSendMessageHandler = (e) => {
+    const onAdminSupportHandler = (e) => {
         let message = messageRef.current.value;
-        if(message.trim()) {
-            console.log(message);
+        if(!message.trim()) {
+            window.confirm("Please enter message");
+            return
         }
+
+        shareSocket.socket?.emit("ADMIN-SEND-MESSAGE-TO-CLIENT", {client, message});
     }
 
     const onChooseAccountSupport = (e) => {
@@ -114,7 +121,7 @@ const PageChat = (props) => {
                                     className={classes['input-message-content']} />
 
                                 <button
-                                    onClick={onSendMessageHandler}
+                                    onClick={onAdminSupportHandler}
                                     className={classes['button-send-message-content']}>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                         <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z" />
